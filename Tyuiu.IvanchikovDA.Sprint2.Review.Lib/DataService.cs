8355d07447f4454dcc1f81affaedf710dd7e924b
Tyuiu.IvanchikovDA@outlook.com <ivanchikov2022@internet.ru>
2023-12-26 14:37:39 +0500

    Sprint2Review

--- Tyuiu.IvanchikovDA.Sprint2.Review.Lib/DataService.cs
+++ Tyuiu.IvanchikovDA.Sprint2.Review.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.IvanchikovDA.Sprint2.Review.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if ((y == x * x) && (y == 2 - x))
+            if ((y <= x * x) && (y <= 2 - x))
             {
                 res = true;
             }
